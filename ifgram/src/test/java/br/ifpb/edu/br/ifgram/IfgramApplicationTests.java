bd20eae9ab9788584b38b7ce3122f274108c5b0f
Rykelme Samuel da Silva Pereira <202412060043@ad.ifpb.edu.br>
2026-09-02 09:55:04 -0300

    Primeiro Commit

---
+++ ifgram/src/test/java/br/ifpb/edu/br/ifgram/IfgramApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifpb.edu.br.ifgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
