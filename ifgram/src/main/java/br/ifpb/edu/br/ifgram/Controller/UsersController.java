28c01bbb52540770643cdf41fa91e1a0835bda62
Rykelme Samuel da Silva Pereira <202412060043@ad.ifpb.edu.br>
2026-09-10 07:52:27 -0300

    primeira criação de rota

--- ifgram/src/main/java/br/ifpb/edu/br/ifgram/Controller/UsersController.java
+++ ifgram/src/main/java/br/ifpb/edu/br/ifgram/Controller/UsersController.java
@@ -1,4 +1,33 @@
 package br.ifpb.edu.br.ifgram.Controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("ryks")
 public class UsersController {
+
+    @GetMapping
+    public String getUser(){
+        return "Olá meu vulgo é kekel";
+    }
+
+    @PostMapping
+    public String postUser(){
+        return "Chamei o endpoint como um POST!";
+    }
+
+    @DeleteMapping
+    public String deleteUser(){
+        return "Chamei o endpoint como um DELETE!";
+    }
+
+    @PatchMapping
+    public String patchUser(){
+        return "Chamei o endpoint como um PATCH!";
+    }
+
+    @PutMapping
+    public String putUser(){
+        return "Chamei o endpoint como um Put!";
+    }
 }
