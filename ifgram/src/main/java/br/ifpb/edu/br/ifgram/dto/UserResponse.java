7840be6794b13c34c716560c8852cce20f68a08e
Rykelme Samuel da Silva Pereira <202412060043@ad.ifpb.edu.br>
2026-09-23 10:07:04 -0300

    criei o User Response

--- ifgram/src/main/java/br/ifpb/edu/br/ifgram/dto/UserResponse.java
+++ ifgram/src/main/java/br/ifpb/edu/br/ifgram/dto/UserResponse.java
@@ -1,4 +1,10 @@
 package br.ifpb.edu.br.ifgram.dto;
 
-public record UserResponse() {
+import org.apache.catalina.User;
+
+public record UserResponse(Long id, String nome, String email) {
+    public static UserResponse from(User user){
+        return new UserResponse(user.getId(),user.getNome(),user.getEmail());
+    }
+
 }
